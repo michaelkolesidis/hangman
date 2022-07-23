9d778d96556192839756b391e83ad19fa32a48ff
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-07-23 14:36:48 +0300

    project reorganization

--- src/Hangman.java
+++ src/Hangman.java
@@ -3,5 +3,5 @@ class Hangman {
     public static void main(String[] args) {
         System.out.println("Hangman");
     }
-    
+
 }
---
+++ src/Wordlist.java
@@ -0,0 +1,11 @@
+public class Wordlist {
+
+    String filename;
+
+    Wordlist(String fileName_) {
+     filename = fileName_;
+    }
+
+    
+
+}
