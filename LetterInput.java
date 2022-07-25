ef1316414df5dcf7dd1f11a680ce11104a149327
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-07-25 15:18:52 +0300

    code cleanup

--- LetterInput.java
+++ LetterInput.java
@@ -5,8 +5,8 @@ public class LetterInput implements Input {
     public String input() {
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner.nextLine().substring(0, 1);
-
         // scanner.close();
+
         return inputString;
     }
 
