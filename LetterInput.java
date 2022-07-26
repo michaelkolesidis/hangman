11f2d685e784176e6b26e90a52c78554595fca42
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-07-26 13:09:42 +0300

    fixed empty input error

--- LetterInput.java
+++ LetterInput.java
@@ -1,13 +1,18 @@
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Scanner;
 
 public class LetterInput implements Input {
 
     public String input() {
         Scanner scanner = new Scanner(System.in);
-        String inputString = scanner.nextLine().substring(0, 1);
-        // scanner.close();
+        String inputString = scanner.nextLine();
+        System.out.println("String was: " + inputString);
+        String outputString = " ";
 
-        return inputString;
+        if (inputString.length() > 0) {
+            outputString = inputString.substring(0, 1);
+        }
+
+        return outputString;
     }
 
 }
